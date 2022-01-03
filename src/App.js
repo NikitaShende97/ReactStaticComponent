19766b88fa149068613d0655522a04f7e2a3045d
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-03 20:32:57 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
-import HelloWorld from "./component/HelloWorld";
 export default function App() {
   return (
     <div className="container-fluid">
-      <div className="row">
-        <div className="col">
-          <HelloWorld></HelloWorld>
+      <div className="row bg-dark text-white sticky-top ">
+        <div className="col d-flex justify-content-center align-items-center p-2">
+          React
         </div>
       </div>
 
-      <div className="row">
-        <div className="col">
-          <HelloWorld></HelloWorld>
+      <div className="row mt-2 ">
+        <div className="col-sm-12 col-md-4 mb-2">
+          <MyCard />
         </div>
+        <div className="col-sm-12 col-md-4 mb-2">
+          <MyCard />
+        </div>
+        <div className="col-sm-12 col-md-4 mb-2">
+          <MyCard />
+        </div>
+        <div className="col-sm-12 col-md-4 mb-2">
+          <MyCard />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+function MyCard() {
+  return (
+    <div className="card">
+      <div className="card-header">Card1</div>
+      <div className="card-body">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, vel
+          eligendi. Laborum debitis, eaque autem voluptatibus quod nihil sint
+          omnis.
+        </p>
+        <input
+          type="button"
+          value="Buy Now"
+          className="btn btn-outline-primary"
+        ></input>
+        <input
+          type="button"
+          value="Add to Cart"
+          className="btn btn-outline-primary"
+        ></input>
       </div>
     </div>
   );
