230852ecc217db9c8ed92e99d2670769ad5dc188
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-03 20:14:33 +0530

    b1

---
+++ src/component/HelloWorld.js
@@ -0,0 +1,12 @@
+export default function HelloWorld() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <p>
+        lorem ggfdd fdg g fdf fdfgdgdd fgdfgd fgdfdgf dgfgdfgd fdgfgdgggggg gf
+        sh gdhsgdhsgd hdgshdgsh dshgdhsgdshdghsgdh hgfdgdddd dgdgdgf dfdgfdg
+        fgdgg fgdfdgf dgfgdfgd fdgfgdgggggg
+      </p>
+    </div>
+  );
+}
