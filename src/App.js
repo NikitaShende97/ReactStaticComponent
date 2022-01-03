2836a989df1d5a9a9b9761733af62cfccc0e417a
NikitaShende97 <nikitashendenn@gmail.com>
2022-01-04 01:46:06 +0530

    b4

--- src/App.js
+++ src/App.js
@@ -1,20 +1,144 @@
+import images from "./images.jpg";
 export default function App() {
-  let list = [1, 1, 1, 1, 1, 1, 1, 1, 1];
   return (
-    <div className="container-fluid">
-      <div className="row bg-dark text-white sticky-top ">
-        <div className="col d-flex justify-content-center align-items-center p-2">
-          React
+    <div>
+      <div className="container-fluid">
+        <HeaderTop />
+        <HeaderSecond />
+        <Body />
+      </div>
+    </div>
+  );
+}
+
+function HeaderTop() {
+  return (
+    <div
+      className="row text-light sticky-top "
+      style={{ backgroundColor: " #E30047" }}
+    >
+      <div className="col-6">Brand wali Quality,Bazaar Wali Deal!</div>
+      <div className="col-6  ">
+        <div className="row">
+          <div className="col mx-0 ">
+            <a
+              href=""
+              className="text-decoration-none "
+              style={{ color: "white" }}
+            >
+              Impact@Snapdeal"
+            </a>
+          </div>
+          <div className="col mx-0">
+            <a
+              href=""
+              style={{ color: "white" }}
+              className="text-decoration-none"
+            >
+              Gift Cards
+            </a>
+          </div>
+          <div className="col mx-0">
+            <a
+              href=""
+              className="text-decoration-none"
+              style={{ color: "white" }}
+            >
+              Help Center
+            </a>
+          </div>
+          <div className="col mx-0">
+            <a
+              href=""
+              className="text-decoration-none"
+              style={{ color: "white" }}
+            >
+              Sell On Snapdeal
+            </a>
+          </div>
+          <div className="col mx-0">
+            <a
+              href=""
+              className="text-decoration-none"
+              style={{ color: "white" }}
+            >
+              Download App
+            </a>
+          </div>
         </div>
       </div>
+    </div>
+  );
+}
 
-      <div className="row mt-2 ">
-        {list.map((item) => (
-          <div className="col-sm-12 col-md-4 mb-2">
+function HeaderSecond() {
+  return (
+    <div
+      className="row text-white p-2  sticky-top"
+      style={{ backgroundColor: " #E30047" }}
+    >
+      <div className="col-2 fs-2">Snapdeal</div>
+      <div className="col-8">
+        <input
+          type="text"
+          placeholder="search products & brands"
+          className="w-75"
+          style={{ height: "35px" }}
+        ></input>
+        <input type="button" value="search" className="btn btn-dark"></input>
+      </div>
+      <div className="col-1">
+        <a className="text-decoration-none text-white" href="">
+          Cart
+        </a>
+      </div>
+      <div className="col-1">
+        <a href="" className="text-decoration-none text-white">
+          Sign In
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Body() {
+  return (
+    <div className="row justify-content-center align-items-center mt-2 ">
+      <div className="col-2 mx-1 d-flex flex-column border sticky-top mt-1">
+        Top Categories
+        <a href="" className="text-decoration-none text-dark mt-2">
+          Men's Fashion
+        </a>
+        <a href="" className="text-decoration-none text-dark mt-2">
+          Men's Fashion
+        </a>
+        <a href="" className="text-decoration-none text-dark mt-2">
+          Home & Kitchen
+        </a>
+        <a href="" className="text-decoration-none text-dark mt-2">
+          Toys,kid's Fashion
+        </a>
+        <a href="" className="text-decoration-none text-dark mt-2">
+          Beauty,Health Fashion
+        </a>
+      </div>
+      <div className="col-8">
+        <div className="row">
+          <div className="col-4">
             <MyCard />
           </div>
-        ))}
+          <div className="col-4">
+            <MyCard />
+          </div>
+          <div className="col-4">
+            <MyCard />
+          </div>
+          <div className="col-4">
+            <MyCard />
+          </div>
+        </div>
       </div>
+      <div className="col-2"></div>
     </div>
   );
 }
@@ -22,23 +146,18 @@ export default function App() {
 function MyCard() {
   return (
     <div className="card">
-      <div className="card-header">Card1</div>
       <div className="card-body">
+        <img src={images} style={{ width: "100%" }}></img>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. A, vel
-          eligendi. Laborum debitis, eaque autem voluptatibus quod nihil sint
-          omnis.
+          The Snapdeal Old Logo Colors with Hex & RGB Codes has 2 colors which
+          are Pacific Blue (#189FCC) and UE Red (#C00309). This color
+          combination was created by user Keshav Naidu
         </p>
-        <input
-          type="button"
-          value="Buy Now"
-          className="btn btn-outline-primary"
-        ></input>
-        <input
-          type="button"
-          value="Add to Cart"
-          className="btn btn-outline-primary"
-        ></input>
+        <div className="mb-1">Rs.120</div>
+        <span>&#128077;</span> &nbsp;&nbsp;
+        <span>0</span>&nbsp;&nbsp;
+        <span>&#128078;</span>&nbsp;&nbsp;
+        <span>0</span>&nbsp;&nbsp;
       </div>
     </div>
   );
